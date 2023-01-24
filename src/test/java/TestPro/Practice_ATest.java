c66a3c0f913ed5553fb0687a36a34998531df4bf
Nagaraj <jorapurnagaraj2@gmail.com>
2023-01-24 14:54:45 +0530

    2nd commit

---
+++ src/test/java/TestPro/Practice_ATest.java
@@ -0,0 +1,12 @@
+package TestPro;
+
+import org.testng.Reporter;
+import org.testng.annotations.Test;
+
+public class Practice_ATest {
+@Test
+public void newonetest() {
+	
+	Reporter.log("new one projet get added", true);
+}
+}
