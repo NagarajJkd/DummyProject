968abe529c51d36008a9cfe1ff343e50fa871b22
Nagaraj <jorapurnagaraj2@gmail.com>
2023-01-24 21:39:36 +0530

    commit 2

---
+++ src/test/java/TestPro/Practice_BTest.java
@@ -0,0 +1,13 @@
+package TestPro;
+
+import org.testng.Reporter;
+import org.testng.annotations.Test;
+
+public class Practice_BTest {
+	
+	@Test
+	public void testB() {
+		Reporter.log("Practice B test");
+	}
+
+}
