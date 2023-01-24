229ca9c15fdbf6efd6455c4f9223e945fd5ced30
Nagaraj <jorapurnagaraj2@gmail.com>
2023-01-24 13:10:45 +0530

    1st commit

---
+++ src/test/java/TestPro/PracticeTest.java
@@ -0,0 +1,14 @@
+package TestPro;
+
+import org.testng.Reporter;
+import org.testng.annotations.Test;
+
+public class PracticeTest {
+	
+@Test
+public void newTest() {
+	Reporter.log("hi", true);
+}
+
+
+}
